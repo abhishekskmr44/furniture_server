a06617f182f20d895f610e5513fbae43d2e01b3b
Abhishek Sukumar <abhishekskmr44@gmail.com>
2022-11-28 15:05:42 +0530

    json practice

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
-
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
+
+
+//tu hi haina
